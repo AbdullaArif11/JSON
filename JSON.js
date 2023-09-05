a091b259f4ef1fed17f4691789c3a9c6bafda1c4
AbdullaArif11 <onlinehero1971@gmail.com>
2023-09-06 04:08:21 +0600

    first commit

---
+++ JSON.js
@@ -0,0 +1,26 @@
+// node JSON.js
+
+const user = {id: 1, name: 'Ami', job: 'coding'};
+const stringified = JSON.stringify(user);
+console.log(user);
+console.log(stringified);
+
+const shop = {
+  Owner: 'Ami',
+  address: {
+    street: 'Bangladesh road',
+    city: 'Bangladesh city',
+    country: 'Bangladesh'
+  },
+  product: ['laptop', 'Desktop', 'Monitor', 'Computer-Hardware'],
+  revenue: 650000,
+  isOpen: true,
+  isNew: false
+}
+
+console.log(shop);
+const shopjson = JSON.stringify(shop)
+console.log(shopjson);
+
+const shopObject = JSON.parse(shopjson);
+console.log(shopObject);
